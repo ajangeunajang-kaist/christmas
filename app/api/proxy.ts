8cbfb98fe4576dff0ffd5e5099a53c23580213c7
장은아 <ajangeunajang@gmail.com>
2025-12-14 02:09:59 +0900

    프록시 수정

--- app/api/proxy.ts
+++ app/api/proxy.ts
@@ -1,19 +1,20 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
-    return res.status(405).end(); // Method Not Allowed
+    return res.status(405).end();
   }
 
   const { id } = req.body;
 
-  // 외부 서버로 전달
-  const extRes = await fetch("외부서버주소", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id }),
-  });
+  const extRes = await fetch(
+    "http://mac-beatles1.kaist.ac.kr:50003/start-job",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id }),
+    }
+  );
 
   if (!extRes.ok) {
     return res.status(extRes.status).json({ message: "Error fetching data from external server" });
